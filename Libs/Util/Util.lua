8c7dbf5fb2944fc703ed12ffc7bff631798975c1
Tony Lagnada <deloyz@gmail.com>
2026-02-07 17:55:51 -0800

    Min and max character for textInput options #17

--- Libs/Util/Util.lua
+++ Libs/Util/Util.lua
@@ -2,12 +2,43 @@
 local ns = select(2, ...).LibIconPicker; if not ns then return end
 
 --- @class Util
-local S  = {}; ns.O.Util = S
+local o = {}; ns.O.Util = o
 
---- @return table|nil Returns a shallow copy of `t`; returns nil if `t` is nil
-function S.Table_ShallowCopy(t)
-    if t == nil then return nil end
-    local t2 = {}
-    for k,v in pairs(t) do t2[k] = v end
-    return t2
+--- @param tbl table
+--- @param shallow boolean
+--- @return table The copied table
+function o.Table_Copy(tbl, shallow)
+  if tbl == nil then return nil end
+  local copy = {};
+  for k, v in pairs(tbl) do
+    if type(v) == "table" and not shallow then
+      copy[k] = o.Table_Copy(v, shallow);
+    else
+      copy[k] = v;
+    end
+  end
+  return copy;
+end
+
+--- Applies non-nil values from {right} over {left}.
+--- {left} values act as defaults.
+--- Returns a new table; inputs are not modified.
+--- @param left table|nil
+--- @param right table
+--- @return table
+function o.Table_MergeWithDefaults(left, right)
+  assert(type(right) == 'table', "The param [right] must be a table.")
+  if left == nil then return o.Table_Copy(right, false) end
+  local result = o.Table_Copy(left, false)
+  
+  -- apply override values over defaults
+  for k, v in pairs(right) do
+    if type(v) == "table" then
+      local destSub = type(result[k]) == "table" and result[k] or nil
+      result[k] = o.Table_MergeWithDefaults(destSub, v)
+    else
+      if v ~= nil then result[k] = v end
+    end
+  end
+  return result
 end
